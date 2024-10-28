a4bdc494d47789287804441a79aba6c6dcc827b1
Lukasz Dziedziczak <wukash@gmail.com>
2024-10-28 12:53:39 +1030

    Setup project, added military character

---
+++ Source/GAD221_CombatMedic/CombatMedic_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CombatMedic_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAD221_COMBATMEDIC_API ACombatMedic_HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
