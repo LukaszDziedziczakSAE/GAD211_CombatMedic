39646c1f2d77f6e94bcee4959b00ea506445ad7a
Lukasz Dziedziczak <wukash@gmail.com>
2024-10-28 12:00:51 +1030

    Basic project setup

---
+++ Source/GAD221_CombatMedic/CombatMedicGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatMedicGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAD221_COMBATMEDIC_API ACombatMedicGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
