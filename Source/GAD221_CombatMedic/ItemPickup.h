84eb840400456b9f12fa94a1339762167446c8cc
Lukasz Dziedziczak <wukash@gmail.com>
2024-11-18 12:20:16 +1030

    Added Item Pickup

---
+++ Source/GAD221_CombatMedic/ItemPickup.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemPickup.generated.h"
+
+UCLASS()
+class GAD221_COMBATMEDIC_API AItemPickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemPickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly)
+	class USphereComponent* Sphere;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+};
